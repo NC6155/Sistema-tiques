0122a36af89631682ced1acaf450880440081c4e
Nicolas <cariaganicolas8@gmail.com>
2024-06-27 16:42:28 -0400

    Agregado el filtrado de tiques

--- JefeMesa/JefeMDB.py
+++ JefeMesa/JefeMDB.py
@@ -1,4 +1,4 @@
-import time
+from os import system
 import mysql.connector
 from tabulate import tabulate
 
@@ -36,7 +36,7 @@ class DatabaseJefe():
                         rutJefe=self.cursor.fetchone()
                 except Exception as err:
                     print(err)
-                sql3="insert into Ejecutivo values("+repr(rutEj)+","+repr(areaEj)+","+repr(nombreEj)+","+repr(contrEj)+","+repr(rutJefe)+");"
+                sql3="insert into Ejecutivo values("+repr(rutEj)+","+repr(areaEj)+","+repr(nombreEj)+","+repr(contrEj)+",A,"+repr(rutJefe)+");"
                 try:
                     self.cursor.execute(sql3)
                     self.conexion.commit()
@@ -47,20 +47,68 @@ class DatabaseJefe():
             print(err)
         return rutJefe
     
-    def generarLista(self):
-        sql1="select * from Tiques"
-        try:
-            self.cursor.execute(sql1)
-            listaTique=self.cursor.fetchall()
-            if listaTique!=None:
-                for tique in range(len(listaTique)):
-                    sql2="select nombreEjec from Ejecutivo where rutEjec="+repr(tique[15])
-                    try:
-                        self.cursor.execute(sql2)
-                        if self.cursor.fetchone()!=None:
-                            nomEjec=self.cursor.fetchone()
-                    except Exception as err:
-                        print(err)
-                    print(tabulate([[tique[0],tique[5],tique[6],tique[9],tique[10],nomEjec]], tablefmt="github"))
-        except Exception as err:
-            print(err)            
+    
+    def filtradoTique(self):
+        while True:
+            system("cls")
+            eleccion=input("Filtrar por\n\
+                    Fecha de creación (F)\n\
+                    Criticidad (C)\n\
+                    Tipo de tique (T)\n\
+                    Área (A)\n\
+                    Ejecutivo que abrió el tique (AT)\n\
+                    Ejecutivo que cerró el tique (CT)\n\
+                    Salir (S)\n\
+                    : ").upper()
+            if eleccion=="F":
+                filtrado=input("Ingrese fecha de creación (dd/mm/aaaa): ")
+                sql2="select * from Tiques where fechaCr=str_to_date("+repr(filtrado)+",'%d/%m/%Y')"
+            elif eleccion=="C":
+                filtrado=input("Ingrese la criticidad de el o los tique(s): ").capitalize()
+                sql2="select * from Tiques where criticidad="+repr(filtrado)
+            elif eleccion=="T":
+                filtrado=input("Ingrese el tipo de tique a buscar\n\
+                            Tipos de tique: felicitación, consulta, reclamo, problema\n\
+                            : ").capitalize()
+                sql2="select * from Tiques where tipoTique="+repr(filtrado)
+            elif eleccion=="A":
+                filtrado=input("Ingrese el área de el o los tique(s): ").capitalize()
+                sql2="select * from Tiques where areaDerivar="+repr(filtrado)
+            elif eleccion=="AT":
+                filtrado=input("Ingrese el rut del ejecutivo que abrió el o los tique(s): ")
+                sql2="select * from Tiques where rutEjecCr="+repr(filtrado)
+            elif eleccion=="CT":
+                filtrado=input("Ingrese el rut del ejecutivo que cerró el o los tique(s): ")
+                sql2="select * from Tiques where rutEjecMo="+repr(filtrado)
+            elif eleccion=="S":
+                break
+            else:
+                pass
+            try:
+                if sql2!=None:
+                    self.cursor.execute(sql2)
+                    listaFilt=self.cursor.fetchall()
+                    for tiquesF in listaFilt:
+                        sql3="select nombreEjec from Ejecutivo where rutEjec="+repr(tiquesF[15])
+                        try:
+                            self.cursor.execute(sql3)
+                            if self.cursor.fetchone()!=None:
+                                nomEjec=self.cursor.fetchone()
+                        except Exception as err:
+                            print(err)
+                        print(tabulate([[tiquesF[0],tiquesF[5],tiquesF[6],tiquesF[9],tiquesF[10],nomEjec]], tablefmt="github"))
+                    while True:
+                        manejoTique=input("¿Desea manejar un tique en específico? (S/N)").upper()
+                        if manejoTique=="S":
+                            pass
+                        else:
+                            break
+                        
+                else:
+                    input("Volviendo al menú principal, presione Enter para continuar...")
+            except Exception as err:
+                    print(err)
+
+    def editarTique(self):
+        idTique=int(input("Ingrese el id del tique: "))
+        
